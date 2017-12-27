fe724e29ce22a428b07787a5118999a52ad33848
amansrivastava89 <aman.srivastava@quovantis.com>
2017-12-27 12:48:30 +0530

    Students Dummy

--- src/main/java/com/thymeleaf/demo/controller/IndexController.java
+++ src/main/java/com/thymeleaf/demo/controller/IndexController.java
@@ -1,5 +1,10 @@
 package com.thymeleaf.demo.controller;
 
+import com.thymeleaf.demo.entity.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +17,25 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("recipient", "World");
         return "index1";
+    }
+	@RequestMapping(value = "/students", method = RequestMethod.GET)
+    public String showStudents(Model model) {
+		List<Student> students = new ArrayList<Student>();
+		// logic to build student data
+		
+		Student student1 = new Student();
+		student1.setId(1);
+		student1.setName("Name 1");
+		students.add(student1);
+		
+		Student student2 = new Student();
+		student2.setId(2);
+		student2.setName("Name 2");
+		students.add(student2);
+		
+		model.addAttribute("students", students);
+        model.addAttribute("recipient", "World");
+        return "students";
     }
 
 }
---
+++ src/main/java/com/thymeleaf/demo/entity/Student.java
@@ -0,0 +1,23 @@
+package com.thymeleaf.demo.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Integer id;
+    private String name;
+    // standard getters and setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
+}
